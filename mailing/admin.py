657d3f1fae8030a975f46cd87ab7d546d275dbf3
Victor Volkov <vektorn1212@gmail.com>
2023-11-02 18:38:59 +0200

    update admin.py: added field owner

--- mailing/admin.py
+++ mailing/admin.py
@@ -5,21 +5,21 @@ from mailing.models import Client, Settings, Message, Logs
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'surname', 'message',)
+    list_display = ('email', 'first_name', 'last_name', 'surname', 'message', 'owner')
 
 
 @admin.register(Settings)
 class MailingSettingsAdmin(admin.ModelAdmin):
     list_display = (
-        'mailing_name', 'mailing_time_start', 'mailing_time_end', 'periodicity', 'mailing_status', 'mailing',
+        'mailing_name', 'mailing_time_start', 'mailing_time_end', 'periodicity', 'mailing_status', 'mailing', 'owner',
     )
 
 
 @admin.register(Message)
 class MailingMessageAdmin(admin.ModelAdmin):
-    list_display = ('mail_subject', 'mail_body',)
+    list_display = ('mail_subject', 'mail_body', 'owner',)
 
 
 @admin.register(Logs)
 class MailingLogsAdmin(admin.ModelAdmin):
-    list_display = ('mailing_time', 'mailing_status', 'mail_server_response', 'mailing')
+    list_display = ('mailing_time', 'mailing_status', 'mail_server_response', 'mailing',)
