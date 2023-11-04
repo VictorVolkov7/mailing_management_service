5c412d3bc2333fa16757122da8c4f1b2294b164d
Victor Volkov <vektorn1212@gmail.com>
2023-11-04 18:53:29 +0200

    added urls.py: added two paths for users_list and toggle_ban_activity for users ban

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from users.apps import UserConfig
-from users.views import LoginView, LogoutView, UserUpdateView, UserCreateView, VerifiedSentView, verify_email
+from users.views import LoginView, LogoutView, UserUpdateView, UserCreateView, VerifiedSentView, verify_email, \
+    UserListView, toggle_ban_activity
 
 app_name = UserConfig.name
 
@@ -12,4 +13,6 @@ urlpatterns = [
     path('register/', UserCreateView.as_view(), name='register'),
     path('register/verified/', VerifiedSentView.as_view(), name='verified'),
     path('verify_email/<str:uidb64>/<str:token>/', verify_email, name='verify_email'),
+    path('users/', UserListView.as_view(), name='users_list'),
+    path('ban_activity/<int:pk>', toggle_ban_activity, name='user_ban'),
 ]
